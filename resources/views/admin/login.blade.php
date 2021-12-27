dcb7104f564d3bc2bb74876c1636057439232d21
daffakurnia11 <72376750+daffakurnia11@users.noreply.github.com>
2021-12-27 20:36:20 +0700

    Update login.blade.php

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -14,7 +14,7 @@
   <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
 
-  <title>Skodash - Bootstrap 5 Admin Template</title>
+  <title>IFEM 2022 | Admin Login</title>
 </head>
 
 <body>

8d90f371d4f038da1a091be0768a545665051c78
daffakurnia11 <72376750+daffakurnia11@users.noreply.github.com>
2022-02-03 17:34:54 +0700

    Fixing Footer Social Media

--- resources/views/main/layouts/footer.blade.php
+++ resources/views/main/layouts/footer.blade.php
@@ -2,21 +2,24 @@
 <footer id="footer">
   <div class="container my-5">
     <h3 class="footer-title">Indonesian Future Vehicle Marathon 2022</h3>
-    <div class="d-flex justify-content-center">
-      <a href="" class="d-inline-block mx-3">
+    <div class="d-flex justify-content-center flex-wrap">
+      <a href="https://www.facebook.com/IFEM-2022-171464176777610" target="_blank" class="d-inline-block mx-2 mx-sm-3 mx-md-4 my-3">
         <i class="footer-logo fab fa-3x fa-facebook-f"></i>
       </a>
-      <a href="" class="d-inline-block mx-3">
+      <a href="https://www.tiktok.com/@ifem2022" target="_blank" class="d-inline-block mx-2 mx-sm-3 mx-md-4 my-3">
         <i class="footer-logo fab fa-3x fa-tiktok"></i>
       </a>
-      <a href="" class="d-inline-block mx-3">
+      <a href="https://www.instagram.com/ifem.2022/" target="_blank" class="d-inline-block mx-2 mx-sm-3 mx-md-4 my-3">
         <i class="footer-logo fab fa-3x fa-instagram"></i>
       </a>
-      <a href="" class="d-inline-block mx-3">
+      <a href="https://twitter.com/lFEM2022" target="_blank" class="d-inline-block mx-2 mx-sm-3 mx-md-4 my-3">
         <i class="footer-logo fab fa-3x fa-twitter"></i>
       </a>
-      <a href="" class="d-inline-block mx-3">
+      <a href="https://www.linkedin.com/company/ifem-2022" target="_blank" class="d-inline-block mx-2 mx-sm-3 mx-md-4 my-3">
         <i class="footer-logo fab fa-3x fa-linkedin-in"></i>
+      </a>
+      <a href="https://www.youtube.com/channel/UCknzEsZWS1XmV7pRpOqV4FQ/featured" target="_blank" class="d-inline-block mx-2 mx-sm-3 mx-md-4 my-3">
+        <i class="footer-logo fab fa-3x fa-youtube"></i>
       </a>
     </div>
   </div>
